cf443ba3e774d4fadedc5a3f866144766ded5b74
usama-bit137 <usamaaqeel98@gmail.com>
2023-06-23 18:35:12 +0100

    '/api/v1/posts' route implemented, dealing with all 'blog post' requests.

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
+const postRouter = require('./routes/postRoutes');
 
 const app = express();
 
+// JSON body parser:
+app.use(express.json());
 app.use(morgan('dev'));
+
+app.use('/api/v1/posts', postRouter);
 
 module.exports = app;

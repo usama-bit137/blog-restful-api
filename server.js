c370422ce2819cb45bc164b480d6784e2970d641
usama-bit137 <usamaaqeel98@gmail.com>
2023-06-23 17:39:42 +0100

    Database connected to server.

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
 const app = require('./app');
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('Database Connection Successful... Happy Blogging!'));
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {

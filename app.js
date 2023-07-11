2afd1792c6537d6566dc7fb3185eda5c6db1b5cd
usama-bit137 <usamaaqeel98@gmail.com>
2023-07-11 16:53:12 +0100

    Implemented environmental variables.

--- app.js
+++ app.js
@@ -6,8 +6,17 @@ const app = express();
 
 // JSON body parser:
 app.use(express.json());
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+
+console.log(`You are in the ${process.env.NODE_ENV} enviroment!`);
 
 app.use('/api/v1/posts', postRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(400).json({
+    status: 'fail',
+    message: `Invalid URL ${req.originalUrl} on this server`,
+  });
+});
 
 module.exports = app;

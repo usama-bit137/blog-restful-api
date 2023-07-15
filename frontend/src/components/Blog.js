747e2123e9371691c06ee7c4ac7370c1bfcb1f8a
usama-bit137 <usamaaqeel98@gmail.com>
2023-07-15 22:56:53 +0100

    Added liking functionality using state

---
+++ frontend/src/components/Blog.js
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import '../styles/Blog.css';
+import heart from '../icons/heart-inactive.png';
+import heartActive from '../icons/heart-active.png';
+
+export default function Blog({ title, bodyText }) {
+  // create some state here:
+  const [liked, setLiked] = useState(false);
+
+  function handleChange() {
+    setLiked((liked) => !liked);
+  }
+
+  return (
+    <div class="blog--container">
+      <h1 className="blog--title">{title}</h1>
+      <a className="blog--preview" href="/">
+        {bodyText}
+      </a>
+      <div className="grid-container">
+        <img
+          src={liked ? heartActive : heart}
+          width="30px"
+          onClick={handleChange}
+          alt={liked ? 'You liked this post' : 'Send a like on this post'}
+        />
+      </div>
+    </div>
+  );
+}

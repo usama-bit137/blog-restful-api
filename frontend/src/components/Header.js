88334dfe653f1dd67902f77fe01a25fa8abc7411
usama-bit137 <usamaaqeel98@gmail.com>
2023-07-15 22:53:03 +0100

    Removed Span for subtitle as not required for now.

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,8 @@
 import '../styles/Header.css';
 export default function Header() {
   return (
-    <div className="header">
+    <div className="header sticky">
       <h1 className="header--title">The Llama Zone </h1>
-      {/* <span style={{ fontSize: 'small', flex: 'wrap' }}>
-        : Full-Stack Developer and Theoretical Physicist
-      </span> */}
       <ul className="header--link-container">
         <Link name="Home" />
         <Link name="About" />

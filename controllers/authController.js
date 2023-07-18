cd89f4176de2a6ca3b337b9f4f6639eec6462843
usama-bit137 <usamaaqeel98@gmail.com>
2023-07-18 16:36:38 +0100

    Completed the signup functionality. Need to include the JWT authorisation.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,17 +1,20 @@
 const User = require('../models/usersModel');
 
 exports.signup = async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const { firstName, lastName, username, email, password, passwordConfirm } =
+    req.body;
+  const newUser = await User.create({
+    firstName,
+    lastName,
+    username,
+    email,
+    password,
+    passwordConfirm,
+  });
   res.status(201).json({
-    status: 'status',
+    status: 'success',
     data: {
       user: newUser,
     },
   });
-};
-
-exports.login = async (req, res, next) => {
-  // 1) Check email and password exists
-  // 2) Check if user exists && password is correct
-  // 3) If everything ok, send token to client
 };

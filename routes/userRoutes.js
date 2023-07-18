f34302d2dfaf8978189638a26ab12051d9251c04
usama-bit137 <usamaaqeel98@gmail.com>
2023-07-18 16:38:37 +0100

    Implemented user signup route using authorisation controller.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const controller = require('../controllers/userController');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
-
+router.post('/signup', authController.signup);
+// router.get('/login', authController.login);
 router.route('/').get(controller.getAllUsers).post(controller.createUser);
 
 module.exports = router;

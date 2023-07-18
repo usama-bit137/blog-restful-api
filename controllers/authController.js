de6f4718ca9682f1f1b5a8bc78424e858cc62d3c
usama-bit137 <usamaaqeel98@gmail.com>
2023-07-18 17:19:19 +0100

    Login functionality implemented using jsonwebtokens.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,11 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/usersModel');
+const AppError = require('../utils/AppError');
+
+const signToken = (id) =>
+  jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
 exports.signup = async (req, res, next) => {
   const { firstName, lastName, username, email, password, passwordConfirm } =
@@ -11,10 +18,32 @@ exports.signup = async (req, res, next) => {
     password,
     passwordConfirm,
   });
+
+  const token = signToken(newUser._id);
+
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },
+  });
+};
+
+exports.login = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password)
+    return next(new AppError('Please provide email and password', 400));
+
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user || !user.correctPassword(password, user.password))
+    return new AppError('Incorrect email or password');
+
+  const token = signToken(user._id);
+
+  res.status(200).json({
+    status: 'success',
+    token,
   });
 };

bb09c7501c34af88670829ed8b0a88c922660fc6
usama-bit137 <usamaaqeel98@gmail.com>
2023-07-18 23:49:44 +0100

    Handler for protected user routes implemented. Uses jwt verify to check if the user is logged in and whether or not they've changed their passwords recently.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,4 @@
+const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
 const User = require('../models/usersModel');
 const AppError = require('../utils/AppError');
@@ -46,4 +47,41 @@ exports.login = async (req, res, next) => {
     status: 'success',
     token,
   });
+};
+
+exports.protect = async (req, res, next) => {
+  // 1) Get the token and check it's there:
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    // eslint-disable-next-line prefer-destructuring
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  if (!token)
+    return next(
+      new AppError('You are not logged in! Please log in to get access', 401)
+    );
+
+  // 2) Verification token
+
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+  console.log(decoded);
+
+  // 3) Check if the user still exists:
+  const freshUser = await User.findById(decoded.id);
+  if (!freshUser)
+    return next(
+      new AppError('The user belonging to this token no longer exists.', 401)
+    );
+
+  // 4) Check if user changed pw after token is issued:
+  if (freshUser.changedPasswordAfter(decoded.iat)) {
+    return next(
+      new AppError('User recently changed password! Please login again', 401)
+    );
+  }
+  next();
 };

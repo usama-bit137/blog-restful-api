493890a39470bd21bd521967ad59800ad209f34c
usama-bit137 <usamaaqeel98@gmail.com>
2023-07-18 17:22:50 +0100

    Implemented /login route which is handled by the authController.login route.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,7 +4,7 @@ const authController = require('../controllers/authController');
 
 const router = express.Router();
 router.post('/signup', authController.signup);
-// router.get('/login', authController.login);
+router.get('/login', authController.login);
 router.route('/').get(controller.getAllUsers).post(controller.createUser);
 
 module.exports = router;

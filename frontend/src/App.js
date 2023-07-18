7348300e007fdaca57a7aa0576dc2d31be2f6fc9
usama-bit137 <usamaaqeel98@gmail.com>
2023-07-18 23:51:40 +0100

    Passed other props to the Blogs component besides just title and bodyText

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import Header from './components/Header';
 import Blog from './components/Blog';
 
+import './styles/Footer.css';
 export default function App() {
   const [allBlogs, setAllBlogs] = useState([]);
   async function fetchBlogs() {
@@ -14,9 +15,16 @@ export default function App() {
     fetchBlogs();
   }, []);
 
+  console.log(allBlogs);
+
   function BlogsFurnish() {
     return allBlogs.map((item) => (
-      <Blog title={item.title} bodyText={item.bodyText} key={item._id} />
+      <Blog
+        title={item.title}
+        bodyText={item.bodyText}
+        likes={item.likes}
+        key={item._id}
+      />
     ));
   }
 
@@ -24,6 +32,26 @@ export default function App() {
     <div className="App">
       <Header />
       {BlogsFurnish()}
+      <Footer />
     </div>
+  );
+}
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <small>
+        <span>
+          If you like what you see here feel free to check out the code on{' '}
+        </span>
+
+        <a
+          href="https://github.com/usama-bit137/blog-restful-api"
+          className="github--link"
+        >
+          GitHub
+        </a>
+      </small>
+    </footer>
   );
 }

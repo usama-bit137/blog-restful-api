e62608155edfb6b8b3548d19804f8138f5871eb8
usama-bit137 <usamaaqeel98@gmail.com>
2023-07-18 23:53:06 +0100

    Received likes prop and stored in state. Clicking like on a blog post likes the blog and updates the number of likes on the frontend

--- frontend/src/components/Blog.js
+++ frontend/src/components/Blog.js
@@ -3,19 +3,25 @@ import '../styles/Blog.css';
 import heart from '../icons/heart-inactive.png';
 import heartActive from '../icons/heart-active.png';
 
-export default function Blog({ title, bodyText }) {
+export default function Blog({ title, bodyText, likes, comments }) {
   // create some state here:
   const [liked, setLiked] = useState(false);
-
+  const [clickLike, setClickLike] = useState(likes);
   function handleChange() {
     setLiked((liked) => !liked);
+
+    if (!liked) setClickLike((clickLike) => clickLike + 1);
+    else if (liked) setClickLike((clickLike) => clickLike - 1);
   }
+
+  // frontend liking thing working, but we need to tell the backend
+  // to do the same!
 
   return (
     <div className="blog--container">
       <h1 className="blog--title">{title}</h1>
       <a className="blog--preview" href="/">
-        {bodyText}
+        {bodyText.split(' ').slice(0, 50).join(' ')}
       </a>
       <div className="grid-container">
         <img
@@ -24,6 +30,7 @@ export default function Blog({ title, bodyText }) {
           onClick={handleChange}
           alt={liked ? 'You liked this post' : 'Send a like on this post'}
         />
+        <span>{clickLike}</span>
       </div>
     </div>
   );

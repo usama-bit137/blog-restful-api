08ed8b38435110d733bf0527d1d39ecfc6a9801b
usama-bit137 <usamaaqeel98@gmail.com>
2023-07-18 17:18:51 +0100

    Implemented the globalErrorHandler which handles a wide variety of errors present in the programme

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const postRouter = require('./routes/postRoutes');
 const userRoutes = require('./routes/userRoutes');
+const AppError = require('./utils/AppError');
+const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
@@ -13,11 +15,14 @@ console.log(`You are in the ${process.env.NODE_ENV} enviroment!`);
 
 app.use('/api/posts', postRouter);
 app.use('/api/users', userRoutes);
+
 app.all('*', (req, res, next) => {
-  res.status(400).json({
-    status: 'fail',
-    message: `Invalid URL ${req.originalUrl} on this server`,
-  });
+  // const err = new Error(`Invalid URL ${req.originalUrl} on this server!`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
+  next(new AppError(`Invalid URL ${req.originalUrl} on this server!`, 404));
 });
+
+app.use(globalErrorHandler);
 
 module.exports = app;

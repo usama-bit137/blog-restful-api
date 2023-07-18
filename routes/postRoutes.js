f8da5c95c57a6d5f7680f952720e89b72a2d7fcb
usama-bit137 <usamaaqeel98@gmail.com>
2023-07-19 00:29:14 +0100

    Implemented protected routes on certain queries.

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const controller = require('../controllers/postController');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
 
-router.route('/').get(controller.getAllPosts).post(controller.createPost);
+router
+  .route('/')
+  .get(controller.getAllPosts)
+  .post(authController.protect, controller.createPost);
 
 router
   .route('/:id')
   .get(controller.getPost)
-  .patch(controller.updatePost)
-  .delete(controller.deletePost);
+  .patch(authController.protect, controller.updatePost)
+  .delete(authController.protect, controller.deletePost);
 
+// restrictTo on patch and delete,
 module.exports = router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,6 +5,12 @@ const authController = require('../controllers/authController');
 const router = express.Router();
 router.post('/signup', authController.signup);
 router.get('/login', authController.login);
+
 router.route('/').get(controller.getAllUsers).post(controller.createUser);
 
+router
+  .route('/:id')
+  .get(controller.getUser)
+  .patch(controller.updateUser)
+  .delete(controller.deleteUser);
 module.exports = router;

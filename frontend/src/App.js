943597407709c8ba4684f4e25ec77b3526d7422c
usama-bit137 <usamaaqeel98@gmail.com>
2023-07-18 03:33:13 +0100

    Fetch from server stored in state and we furnish the frontend with the blogs.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,29 @@
-// import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Header from './components/Header';
 import Blog from './components/Blog';
-export default function App() {
-  // API request working...
-  // const APIFetch = async () => {
-  //   let blogs = await fetch('/api/v1/posts');
-  //   const blogsData = await blogs.json();
-  //   return blogsData.posts;
-  // };
 
-  // const blogsFurnish = () => {
-  //   APIFetch().then((res) =>
-  //     res.posts.map((item) => {
-  //       return <Blog title={item.title} bodyText={item.bodyText} />;
-  //     })
-  //   );
-  // };
+export default function App() {
+  const [allBlogs, setAllBlogs] = useState([]);
+  async function fetchBlogs() {
+    await fetch('api/posts')
+      .then((res) => res.json())
+      .then((res) => setAllBlogs(res.posts.posts));
+  }
+
+  useEffect(() => {
+    fetchBlogs();
+  }, []);
+
+  function BlogsFurnish() {
+    return allBlogs.map((item) => (
+      <Blog title={item.title} bodyText={item.bodyText} />
+    ));
+  }
 
   return (
     <div className="App">
       <Header />
-      {/* {blogsFurnish} */}
-      <Blog
-        title="Lorem Ipsum Dolor"
-        bodyText="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras fringilla ac mauris a ullamcorper. Vestibulum varius maximus metus vitae volutpat. In hac habitasse platea dictumst. Donec sagittis sit amet quam ut bibendum. Duis tempor diam ut dui mollis finibus. Suspendisse eget pellentesque mi. Fusce sagittis sed mauris et mattis. Donec rutrum eu neque nec vehicula. Ut leo dui, vehicula vitae tempor ut, aliquam a mauris. Praesent gravida purus non blandit dapibus. Proin elementum est at bibendum lobortis. Nullam maximus, sapien auctor aliquet condimentum, nisi enim sagittis quam, tristique malesuada nisi sem dignissim orci. Nulla vel eros sapien. Duis arcu nisl, bibendum eget elementum vitae, ultricies a justo. Cras ullamcorper enim a mauris finibus, non interdum leo fermentum. Nulla consequat tristique purus."
-      />
-      <Blog
-        title="Lorem Ipsum Dolor"
-        bodyText="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras fringilla ac mauris a ullamcorper. Vestibulum varius maximus metus vitae volutpat. In hac habitasse platea dictumst. Donec sagittis sit amet quam ut bibendum. Duis tempor diam ut dui mollis finibus. Suspendisse eget pellentesque mi. Fusce sagittis sed mauris et mattis. Donec rutrum eu neque nec vehicula. Ut leo dui, vehicula vitae tempor ut, aliquam a mauris. Praesent gravida purus non blandit dapibus. Proin elementum est at bibendum lobortis. Nullam maximus, sapien auctor aliquet condimentum, nisi enim sagittis quam, tristique malesuada nisi sem dignissim orci. Nulla vel eros sapien. Duis arcu nisl, bibendum eget elementum vitae, ultricies a justo. Cras ullamcorper enim a mauris finibus, non interdum leo fermentum. Nulla consequat tristique purus."
-      />
-      <Blog
-        title="Lorem Ipsum Dolor"
-        bodyText="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras fringilla ac mauris a ullamcorper. Vestibulum varius maximus metus vitae volutpat. In hac habitasse platea dictumst. Donec sagittis sit amet quam ut bibendum. Duis tempor diam ut dui mollis finibus. Suspendisse eget pellentesque mi. Fusce sagittis sed mauris et mattis. Donec rutrum eu neque nec vehicula. Ut leo dui, vehicula vitae tempor ut, aliquam a mauris. Praesent gravida purus non blandit dapibus. Proin elementum est at bibendum lobortis. Nullam maximus, sapien auctor aliquet condimentum, nisi enim sagittis quam, tristique malesuada nisi sem dignissim orci. Nulla vel eros sapien. Duis arcu nisl, bibendum eget elementum vitae, ultricies a justo. Cras ullamcorper enim a mauris finibus, non interdum leo fermentum. Nulla consequat tristique purus."
-      />
+      {BlogsFurnish()}
     </div>
   );
 }

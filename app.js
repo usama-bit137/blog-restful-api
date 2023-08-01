20b6a06e5d55b4247c1f23cd141944f3f5cfe732
usama-bit137 <usamaaqeel98@gmail.com>
2023-08-02 00:45:39 +0100

    Set view engine to pug. Set up views router to handle requests from the root folder.

--- app.js
+++ app.js
@@ -1,25 +1,31 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const postRouter = require('./routes/postRoutes');
-const userRoutes = require('./routes/userRoutes');
+const userRouter = require('./routes/userRoutes');
+const viewsRouter = require('./routes/viewsRoutes');
 const AppError = require('./utils/AppError');
 const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
 // JSON body parser:
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'views')));
 app.use(express.json());
+
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
 console.log(`You are in the ${process.env.NODE_ENV} enviroment!`);
 
 app.use('/api/posts', postRouter);
-app.use('/api/users', userRoutes);
+app.use('/api/users', userRouter);
+
+app.use('/', viewsRouter);
 
 app.all('*', (req, res, next) => {
-  // const err = new Error(`Invalid URL ${req.originalUrl} on this server!`);
-  // err.status = 'fail';
-  // err.statusCode = 404;
   next(new AppError(`Invalid URL ${req.originalUrl} on this server!`, 404));
 });
 

544930754c0f199ff86666bfa820d2e44a94c308
usama-bit137 <usamaaqeel98@gmail.com>
2023-08-02 17:14:39 +0100

    Passed a mongoose query to retrieve all posts.

--- routes/viewsRoutes.js
+++ routes/viewsRoutes.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const Post = require('../models/postModel');
+const factory = require('../controllers/viewsFactory');
 
 const router = express.Router();
 
@@ -6,10 +8,19 @@ router.get('/', (req, res) => {
   res.status(200).render('base');
 });
 
-router.get('/all', (req, res) => {
+router.get('/all', async (req, res) => {
+  const posts = await Post.find();
   res.status(200).render('blogs', {
     title: 'All Posts',
+    posts,
   });
 });
 
+// Cleaner:
+router.get('/home', factory.getPage('Home'));
+router.get('/about', factory.getPage('About'));
+router.get('/contact', factory.getPage('Contact'));
+router.get('/login', factory.getPage('Login'));
+router.get('/signup', factory.getPage('Signup'));
+router.get('/new', factory.getPage('New'));
 module.exports = router;

49335768b0d74dbe6488a63441f473b4f21fc2c5
usama-bit137 <usamaaqeel98@gmail.com>
2023-08-03 15:19:14 +0100

    Views handlers moved into viewsFactory function

--- controllers/viewsFactory.js
+++ controllers/viewsFactory.js
@@ -1,5 +1,30 @@
-exports.getPage = (page) => (req, res) => {
-  res.status(200).render(page.toLowerCase(), {
-    title: page,
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/AppError');
+const Post = require('../models/postModel');
+
+exports.getPage =
+  (...titleInfo) =>
+  (req, res) => {
+    if (titleInfo.length < 2) titleInfo.push(titleInfo[0].toLowerCase());
+    res.status(200).render(titleInfo[1], {
+      title: titleInfo[0],
+    });
+  };
+
+exports.getAllPosts = catchAsync(async (req, res) => {
+  const posts = await Post.find();
+  res.status(200).render('blogs', {
+    title: 'All Posts',
+    posts,
   });
-};
+});
+
+exports.getPostBySlug = catchAsync(async (req, res, next) => {
+  const post = await Post.findOne({ slug: req.params.slug });
+  if (!post)
+    return next(new AppError('The resource you requested does not exist', 404));
+  res.status(200).render('blog-post', {
+    title: `Post: ${post.title}`,
+    post,
+  });
+});

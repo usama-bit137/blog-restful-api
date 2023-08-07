8455de1fca8c72bf668305637bcffb8c7d95c73e
usama-bit137 <usamaaqeel98@gmail.com>
2023-08-07 22:49:43 +0100

    Cookie parser npm package added which stores jwt in a secure http only location

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
+
 const postRouter = require('./routes/postRoutes');
 const userRouter = require('./routes/userRoutes');
 const viewsRouter = require('./routes/viewsRoutes');
@@ -25,12 +27,19 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json({ limit: '10kb' }));
+app.use(cookieParser());
 app.use(mongoSanitize());
 app.use(xss());
 // Prevent Parameter Pollution
 app.use(hpp());
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 console.log(`You are in the ${process.env.NODE_ENV} enviroment!`);
+
+app.use((req, res, next) => {
+  console.log(req.cookies);
+  next();
+});
+
 app.use('/api/posts', postRouter);
 app.use('/api/users', userRouter);
 app.use('/', viewsRouter);

a2f0edaeaed0272170d584f4cbd43b3019330b2f
usama-bit137 <usamaaqeel98@gmail.com>
2023-08-07 23:01:48 +0100

    Improved security of user routes that are only accessible to admin.

--- routes/viewsRoutes.js
+++ routes/viewsRoutes.js
@@ -1,17 +1,26 @@
 const express = require('express');
 const factory = require('../controllers/viewsFactory');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
+router.use(authController.isLoggedIn);
+
+router.get('/', factory.getPage('Homepage', 'base'));
+router.get('/signup', factory.getPage('Sign up for a new account', 'signup'));
+router.get('/login', factory.getPage('Log in to your account', 'login'));
 
 router.get('/posts', factory.getAllPosts);
-router.get('/posts/:slug', factory.getPostBySlug);
 
-// Cleaner:
-router.get('/', factory.getPage('Homepage', 'base'));
+router.get(
+  '/posts/new',
+  authController.protect,
+  authController.restrictTo('admin'),
+  factory.getPage('New post', 'new')
+);
+
+router.get('/posts/:slug', factory.getPost);
+
 router.get('/about', factory.getPage('About'));
 router.get('/contact', factory.getPage('Contact'));
-router.get('/login', factory.getPage('Log in to your account', 'login'));
-router.get('/signup', factory.getPage('Sign up for a new account', 'signup'));
-router.get('/new', factory.getPage('New post', 'new'));
 
 module.exports = router;

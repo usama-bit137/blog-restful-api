1398cfbfadfe3dce4bac1b8393ef9dd3e1285692
usama-bit137 <usamaaqeel98@gmail.com>
2023-08-08 23:38:48 +0100

    Removed 'About' page as redundant.

--- routes/viewsRoutes.js
+++ routes/viewsRoutes.js
@@ -5,22 +5,24 @@ const authController = require('../controllers/authController');
 const router = express.Router();
 router.use(authController.isLoggedIn);
 
-router.get('/', factory.getPage('Homepage', 'base'));
+router.get('/', factory.getPage('Home', 'base'));
+
+// user account creation routes
 router.get('/signup', factory.getPage('Sign up for a new account', 'signup'));
 router.get('/login', factory.getPage('Log in to your account', 'login'));
 
+// searching posts
 router.get('/posts', factory.getAllPosts);
+router.get('/posts/:slug', factory.getPost);
 
+router.get('/contact', factory.getPage('Contact'));
+
+// admin-oly routes
 router.get(
   '/posts/new',
   authController.protect,
   authController.restrictTo('admin'),
   factory.getPage('New post', 'new')
 );
-
-router.get('/posts/:slug', factory.getPost);
-
-router.get('/about', factory.getPage('About'));
-router.get('/contact', factory.getPage('Contact'));
 
 module.exports = router;

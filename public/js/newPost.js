af35df6cb664908a0fe53046c04d7a0b0b270dcf
usama-bit137 <usamaaqeel98@gmail.com>
2023-08-09 13:54:56 +0100

    Admin is redirected to the '/posts' route after creating a new post.

--- public/js/newPost.js
+++ public/js/newPost.js
@@ -1,5 +1,10 @@
 import axios from 'axios';
+// import EasyMDE from 'easymde';
 
 export const newPost = async (title, bodyText) => {
   axios.post('/api/posts', { title, bodyText }).then((res) => alert(res));
+  window.location.replace('/posts');
 };
+
+// const easyMDE = new EasyMDE({ element: document.getElementById('bodyText') });
+// console.log(easyMDE.value('New Input for EasyMDE'));
